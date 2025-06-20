712398b31665dad0cf1b4a144aa9d754d35af488
RaviV <rvideveloper123@gmail.com>
2025-06-20 11:16:44 +0400

    poc code only

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -8,8 +8,7 @@ const projects = [
     description:
       "Fully cross-platform property management app for Damac customers.",
     link: "https://apps.apple.com/us/app/damac-living/id1446489552",
-    thumbnail:
-      "https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/25/4b/3f/254b3f36-7b28-9d50-f49a-2d96c0e5298a/AppIcon-0-1x_U007emarketing-0-0-85-220-0-7.png/246x0w.png",
+    thumbnail:require('../assets/dL.png'),
   },
   {
     id: 2,
@@ -18,8 +17,7 @@ const projects = [
     description:
       "Robust real estate agent productivity app with complex form flows.",
     link: "https://apps.apple.com/us/app/damac-agents/id1477456157",
-    thumbnail:
-      "https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/fb/35/f0/fb35f0a3-7cd7-008d-7684-d18ca87a1e36/AppIcon-0-1x_U007emarketing-0-0-85-220-0-7.png/246x0w.png",
+    thumbnail:require('../assets/DA.png'),
   },
   {
     id: 3,
@@ -27,8 +25,8 @@ const projects = [
     platform: "React.js",
     description: "Internal-use website for managing chairman profile.",
     link: "https://hussainsajwani.com/",
-    thumbnail:
-      "https://hussainsajwani.com/favicon.ico", // No app icon, so using favicon
+    thumbnail:require('../assets/hasni.png'),
+       // No app icon, so using favicon
   },
   {
     id: 4,
@@ -46,8 +44,7 @@ const projects = [
     description:
       "Healthcare lifestyle tracker for bariatric patients with reminders.",
     link: "https://apps.apple.com/us/app/baritastic-bariatric-tracker/id899131093",
-    thumbnail:
-      "https://is1-ssl.mzstatic.com/image/thumb/Purple122/v4/23/67/49/2367496c-ec7c-646f-5222-0adf45bb3bc4/AppIcon-0-1x_U007emarketing-0-7-0-85-220.png/246x0w.png",
+    thumbnail:require('../assets/bar.png'),
   },
   {
     id: 6,
@@ -56,8 +53,7 @@ const projects = [
     description:
       "Fantasy esports platform with real-time match updates and chat.",
     link: "https://apps.apple.com/in/app/profeud-fantasy-esports/id1510937024",
-    thumbnail:
-      "https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/fd/4b/36/fd4b36bc-6c9d-73a1-423b-0a63d2a01556/AppIcon-0-1x_U007emarketing-0-0-85-220-0-7.png/246x0w.png",
+    thumbnail:require('../assets/dL.png'),
   },
   {
     id: 7,
@@ -66,8 +62,7 @@ const projects = [
     description:
       "Phonebook-based social app with group chats and contact syncing.",
     link: "https://apps.apple.com/us/app/spincontact/id1468458105",
-    thumbnail:
-      "https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/b6/1a/2e/b61a2e42-bc53-269d-2df7-486d06f4a98a/AppIcon-0-1x_U007emarketing-0-0-85-220-0-7.png/246x0w.png",
+    thumbnail:require('../assets/DA.png'),
   },
   {
     id: 8,
@@ -76,8 +71,7 @@ const projects = [
     description:
       "Money transfer features: KYC, bank linking, transaction history.",
     link: "https://play.google.com/store/apps/details?id=com.paysend.app&hl=en",
-    thumbnail:
-      "https://play-lh.googleusercontent.com/mE6GZ_cqvldZpkO6NOKF_mi1dm5rPK92FZoHLym0zNlnJKmn9B8vF7ztFQiH7T_kKiA=s180-rw",
+    thumbnail:require('../assets/pay.png'),
   },
   {
     id: 9,
@@ -86,8 +80,7 @@ const projects = [
     description:
       "Education platform with virtual classes, chat, exams, and payments.",
     link: "https://apps.apple.com/in/app/nactus/id1090907884",
-    thumbnail:
-      "https://is5-ssl.mzstatic.com/image/thumb/Purple62/v4/11/9e/f6/119ef62e-6c4f-06ec-1e6e-3d37c8f3341d/AppIcon-0-1x_U007emarketing-0-0-85-220-0-7.png/246x0w.png",
+    thumbnail:require('../assets/bar.png'),
   },
 ];
 
